859b6260ecf101d6e0d62dabbbb86fdcc8b35238
Erfan Khadivar <rendinjast@gmail.com>
2021-08-21 05:39:57 +0430

    fix number zero

--- dist/assets/index.7c5209a6.js
+++ dist/assets/index.7c5209a6.js
@@ -37,7 +37,7 @@ var e=Object.defineProperty,t=Object.defineProperties,r=Object.getOwnPropertyDes
       height: 9px;
     }
   }
-`,S=({rotate:e})=>{const[t,r]=p.exports.useState(),{state:n}=w();return p.exports.useEffect((()=>{r("light"!==n.theme)}),[n.theme]),g(j,{borderColor:t?"#eeeeee47":"rgba(0,0,0,.2)",children:e.map(((e,r)=>g(E,{color:t?"#eee":"rgb(66, 66, 66)",rotate:e},r)))})},C=[45,45],$=[0,90],O=[0,180],k=[0,270],I=[90,180],H=[90,270],P=[180,270],M=[C,C,P,I,k,$,P,I,k,$,C,C],A=[C,P,H,I,C,O,P,$,C,O,O,C,C,O,O,C,P,$,k,I,k,H,H,$],N=[P,H,H,I,O,P,H,$,O,k,H,I,k,H,I,O,P,H,$,O,k,H,H,$],G=[P,H,H,I,O,P,H,$,O,k,H,I,O,P,H,$,O,k,H,I,k,H,H,$],_=[P,I,P,I,O,O,O,O,O,k,$,O,O,P,H,$,O,O,C,C,k,$,C,C],z=[P,H,H,I,k,H,I,O,P,H,$,O,O,P,H,$,O,k,H,I,k,H,H,$],D=[C,C,P,I,C,C,O,O,P,H,$,O,O,P,I,O,O,k,$,O,k,H,H,$],R=[P,H,H,I,O,P,H,$,O,O,C,C,O,O,C,C,O,O,C,C,k,$,C,C],T=[P,H,H,I,O,P,I,O,O,k,$,O,O,P,I,O,O,k,$,O,k,H,H,$],B=[P,H,H,I,O,P,I,O,O,k,$,O,O,P,H,$,O,O,C,C,k,$,C,C],F=e=>{switch(e){case"0":case"1":return A;case"2":return N;case"3":return G;case"4":return _;case"5":return z;case"6":return D;case"7":return R;case"8":return T;case"9":return B;default:return M}},q=c.div`
+`,S=({rotate:e})=>{const[t,r]=p.exports.useState(),{state:n}=w();return p.exports.useEffect((()=>{r("light"!==n.theme)}),[n.theme]),g(j,{borderColor:t?"#eeeeee47":"rgba(0,0,0,.2)",children:e.map(((e,r)=>g(E,{color:t?"#eee":"rgb(66, 66, 66)",rotate:e},r)))})},C=[45,45],$=[0,90],O=[0,180],k=[0,270],I=[90,180],H=[90,270],P=[180,270],M=[C,C,P,I,k,$,P,I,k,$,C,C],A=[P,H,H,I,O,P,I,O,O,O,O,O,O,O,O,O,O,k,$,O,k,H,H,$],N=[C,P,H,I,C,O,P,$,C,O,O,C,C,O,O,C,P,$,k,I,k,H,H,$],G=[P,H,H,I,O,P,H,$,O,k,H,I,k,H,I,O,P,H,$,O,k,H,H,$],_=[P,H,H,I,O,P,H,$,O,k,H,I,O,P,H,$,O,k,H,I,k,H,H,$],z=[P,I,P,I,O,O,O,O,O,k,$,O,O,P,H,$,O,O,C,C,k,$,C,C],D=[P,H,H,I,k,H,I,O,P,H,$,O,O,P,H,$,O,k,H,I,k,H,H,$],R=[C,C,P,I,C,C,O,O,P,H,$,O,O,P,I,O,O,k,$,O,k,H,H,$],T=[P,H,H,I,O,P,H,$,O,O,C,C,O,O,C,C,O,O,C,C,k,$,C,C],B=[P,H,H,I,O,P,I,O,O,k,$,O,O,P,I,O,O,k,$,O,k,H,H,$],F=[P,H,H,I,O,P,I,O,O,k,$,O,O,P,H,$,O,O,C,C,k,$,C,C],q=e=>{switch(e){case"0":return A;case"1":return N;case"2":return G;case"3":return _;case"4":return z;case"5":return D;case"6":return R;case"7":return T;case"8":return B;case"9":return F;default:return M}},J=c.div`
   display: grid;
   justify-content: center;
   grid-template-columns: repeat(${({separator:e})=>e}, 46px);
@@ -76,23 +76,23 @@ var e=Object.defineProperty,t=Object.defineProperties,r=Object.getOwnPropertyDes
       grid-template-rows: repeat(6, 20px);
     }
   } */
-`,J=({number:e})=>g(q,{separator:4,children:e.map(((e,t)=>g(S,{rotate:e},t)))}),K=()=>g(q,{separator:2,children:M.map(((e,t)=>g(S,{rotate:e},t)))}),L=c.div`
+`,K=({number:e})=>g(J,{separator:4,children:e.map(((e,t)=>g(S,{rotate:e},t)))}),L=()=>g(J,{separator:2,children:M.map(((e,t)=>g(S,{rotate:e},t)))}),Q=c.div`
   position: fixed;
   width: 100%;
   display: flex;
   justify-content: flex-end;
   align-items: center;
-`,Q=c.button`
+`,U=c.button`
   font-size: 1.4rem;
   background: none;
   border: none;
   margin: 12px;
   outline: none;
-`,U=()=>{const{state:e,dispatch:t}=w();return g(L,{children:g(Q,{onClick:()=>{t({type:"CHANGE_THEME",payload:"light"===e.theme?"dark":"light"})},children:"light"===e.theme?"🌝":"🌚"})})},V=c.div`
+`,V=()=>{const{state:e,dispatch:t}=w();return g(Q,{children:g(U,{onClick:()=>{t({type:"CHANGE_THEME",payload:"light"===e.theme?"dark":"light"})},children:"light"===e.theme?"🌝":"🌚"})})},W=c.div`
   background-color: ${({backgroundColor:e})=>e};
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   transition: background-color 1s ease-out;
-`;function W(){const[e,t]=p.exports.useState(),{state:r}=w();return p.exports.useEffect((()=>{const e=setInterval((()=>{const e=new Date;let r=e.getHours().toString().split(""),n=e.getMinutes().toString().split(""),a=e.getSeconds().toString().split("");1===r.length&&(r=["0",r[0]]),1===n.length&&(n=["0",n[0]]),1===a.length&&(a=["0",a[0]]),t({hour:[r[0],r[1]],minute:[n[0],n[1]],second:[a[0],a[1]]})}),1e3);return()=>clearInterval(e)}),[]),x(b,{children:[g(U,{}),x(V,{backgroundColor:"light"===r.theme?"#f7f7f7":"#0e0e0e",children:[g(J,{number:F((null==e?void 0:e.hour[0])||"0")}),g(J,{number:F((null==e?void 0:e.hour[1])||"0")}),g(K,{}),g(J,{number:F((null==e?void 0:e.minute[0])||"0")}),g(J,{number:F((null==e?void 0:e.minute[1])||"0")}),g(K,{}),g(J,{number:F((null==e?void 0:e.second[0])||"0")}),g(J,{number:F((null==e?void 0:e.second[1])||"0")})]})]})}m.render(g(u.StrictMode,{children:g(v,{children:g(W,{})})}),document.getElementById("root"));
+`;function X(){const[e,t]=p.exports.useState(),{state:r}=w();return p.exports.useEffect((()=>{const e=setInterval((()=>{const e=new Date;let r=e.getHours().toString().split(""),n=e.getMinutes().toString().split(""),a=e.getSeconds().toString().split("");1===r.length&&(r=["0",r[0]]),1===n.length&&(n=["0",n[0]]),1===a.length&&(a=["0",a[0]]),t({hour:[r[0],r[1]],minute:[n[0],n[1]],second:[a[0],a[1]]})}),1e3);return()=>clearInterval(e)}),[]),x(b,{children:[g(V,{}),x(W,{backgroundColor:"light"===r.theme?"#f7f7f7":"#0e0e0e",children:[g(K,{number:q((null==e?void 0:e.hour[0])||"0")}),g(K,{number:q((null==e?void 0:e.hour[1])||"0")}),g(L,{}),g(K,{number:q((null==e?void 0:e.minute[0])||"0")}),g(K,{number:q((null==e?void 0:e.minute[1])||"0")}),g(L,{}),g(K,{number:q((null==e?void 0:e.second[0])||"0")}),g(K,{number:q((null==e?void 0:e.second[1])||"0")})]})]})}m.render(g(u.StrictMode,{children:g(v,{children:g(X,{})})}),document.getElementById("root"));
